b169f21b747af85061bc7f7e22ac9b9cd9d7244d
ananthvk <123456789987654321sr@gmail.com>
2024-02-24 00:44:10 +0530

    Added comments to sql

--- backend/src/models/create_tables.sql
+++ backend/src/models/create_tables.sql
@@ -1,4 +1,6 @@
 -- Create a database called wtracker
+
+-- Holds login information of a user
 create table if not exists usr(
     id bigserial primary key,
     name text not null,
@@ -7,12 +9,15 @@ create table if not exists usr(
     salt text not null
 );
 
+-- Contains information regarding a single workout
+-- Say a pushup, or squat or any other exercise
 create table if not exists workout(
     id bigserial primary key,
     name text not null,
     description text
 );
 
+-- Type of field, Rep&Weight, Rep&Time, Time&Calories, etc
 create table if not exists field_type(
     id bigserial primary key,
     name text not null
@@ -20,6 +25,10 @@ create table if not exists field_type(
 
 create index if not exists field_type_name_index ON field_type(name);
 
+-- A tracker is created by the user to track a particular exercise
+-- I decided to keep the tracker separate from workout model, since a user may want
+-- to customize tracking, such as changing the input fields, etc
+-- This also allows the user to create multiple trackers for a single workout
 create table if not exists tracker(
     id bigserial primary key,
     field_type_id bigint references field_type(id) on delete restrict,
@@ -27,6 +36,7 @@ create table if not exists tracker(
     workout_id bigint references workout(id) on delete restrict
 );
 
+-- This table stores details about a single exercise
 create table if not exists workout_log(
     id bigserial primary key,
     date_added timestamp with time zone not null default now(),
@@ -35,6 +45,7 @@ create table if not exists workout_log(
     usr_id bigint references usr(id) on delete cascade
 );
 
+-- Contains information about a single set
 create table if not exists workout_set(
     id bigserial primary key,
     time_field interval,
@@ -42,3 +53,8 @@ create table if not exists workout_set(
     number_field2 integer,
     workout_log_id bigint references workout_log(id) on delete cascade
 );
+
+-- TODO: Implement workout lists
+-- Enable users to create custom workouts
+-- Have a is_created_by_admin boolean flag in the models to 
+-- differentiate between user created ones
