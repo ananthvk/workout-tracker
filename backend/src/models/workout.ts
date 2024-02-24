7e0cd92dfbd0f615b846c544056f6a62dd46f2a3
ananthvk <123456789987654321sr@gmail.com>
2024-02-24 22:53:56 +0530

    finished implementing complete get for a single workout

--- backend/src/models/workout.ts
+++ backend/src/models/workout.ts
@@ -24,10 +24,22 @@ const getWorkout = async (fastify: FastifyInstance, workoutId: String): Promise<
             'SELECT id, name, description FROM workout WHERE id = $1',
             [workoutId]
         )
-        return rows[0];
+        const workoutDetails = rows[0];
+
+        // Get other properties from the M2M tables
+        const musclesQuery = `SELECT muscle.id, muscle.name from muscle INNER JOIN muscle_workout_relation AS ma ON muscle.id = ma.muscle_id WHERE ma.workout_id = $1; `;
+        const muscles = await fastify.pg.query(musclesQuery, [workoutId]);
+        workoutDetails['muscles'] = muscles.rows;
+
+        const workoutTypeQuery = `SELECT workout_type.id, workout_type.name from workout_type INNER JOIN workout_type_workout_relation AS w ON workout_type.id = w.workout_type_id WHERE w.workout_id = $1; `;
+        const workoutTypes = await fastify.pg.query(workoutTypeQuery, [workoutId]);
+        workoutDetails['type'] = workoutTypes.rows;
+        return workoutDetails;
+
     } catch (err) {
         return null;
     }
 }
+
 
 export { getAllWorkouts, getWorkout };
