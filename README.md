cfa20d82f2e9d9ea2a06e014ac7c2b2c1793db03
ananthvk <123456789987654321sr@gmail.com>
2024-03-17 23:03:59 +0530

    Added readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# workout-tracker
+## Overview
+The primary purpose of this application is to track and derive insights into workout data. After a workout, people do not usually record or track details about their workout, such as weights used, reps performed, or total workout time since it is time consuming to do so. These details are crucial to record progress. This app can help to provide details and suggestions to enable the user to reach their goal.
+
+## Homepage
+[https://github.com/ananthvk/workout-tracker](https://github.com/ananthvk/workout-tracker)
+
+## Features
+- Web based app
+
+## Technical
+- Backend: Nodejs (fastify)
+- Fronted: Svelte
+- Deployment: Docker
+- Database: PostgreSQL
+- CSS: TailwindCSS
+
+## Getting started
+### Pre-requisities
+Install [Docker](https://docs.docker.com/engine/install/) and [docker-compose](https://docs.docker.com/compose/migrate/) 
+### Installation
+Clone the repository
+```
+$ git clone https://github.com/ananthvk/workout-tracker/
+```
+Run the application with
+```
+$ docker compose up
+```
+View the application at [http://localhost:3001](http://localhost:3001)
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- backend/src/routes/api/v1/tracker.ts
+++ backend/src/routes/api/v1/tracker.ts
@@ -1,43 +1,7 @@
-import { FastifyPluginAsync, FastifyRequest, RouteShorthandOptions } from "fastify";
-import { getTracker, getTrackers } from "../../../models/tracker.js";
+import { FastifyPluginAsync } from "fastify";
+import { getTrackers } from "../../../models/tracker.js";
 
 const routes: FastifyPluginAsync = async (fastify, opts) => {
-    type TrackerGetRequest = FastifyRequest<{ Params: { id: string } }>
-    const opt: RouteShorthandOptions = {
-        schema: {
-            params: { type: 'object', properties: { id: { type: 'string' } } }/*,
-
-            response: {
-                200: {
-                    type: 'object',
-                    properties: {
-
-                    }
-                },
-                default: {
-                    type: 'object',
-                    properties: {
-                        message: { type: 'string' }
-                    }
-                }
-            }
-            */
-        },
-        onRequest: [fastify.authenticate]
-    };
-
-    // Todo: Make sure that a user cannot access trackers of another user
-    /*
-    fastify.get('/tracker/:id', opt, async (req, reply) => {
-        const id = (req as TrackerGetRequest).params.id
-        const trk = await getTracker(fastify, id);
-        if (trk === null) {
-            fastify.log.info('Not found')
-            reply.notFound('Tracker not found')
-        }
-        return trk
-    })
-    */
 
     fastify.get('/trackers', { onRequest: [fastify.authenticate] }, async (req, reply) => {
         if (isUser(req.user)) {
